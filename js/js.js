e3cb709e1fe6eb8c06079349d5671a8e9831851c
JiaqiGao <jiaqigao12@gmail.com>
2018-02-10 17:31:09 -0600

    update

--- js/js.js
+++ js/js.js
@@ -1,52 +1,145 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth/2, window.innerHeight/2, false );
-document.body.appendChild( renderer.domElement );
-
-var textureLoader = new THREE.TextureLoader();
-
-var planet = {
-    sphere: function(size){
-        let sphere = new THREE.SphereGeometry(size, 32, 32);
-    }
-    return sphere;
-}
-/*
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-
-scene.add( cube );
-*/
-var geometry   = new THREE.SphereGeometry(0.5, 32, 32)
-var material  = new THREE.MeshPhongMaterial()
-var earthMesh = new THREE.Mesh(geometry, material)
-scene.add(earthMesh)
-material.map  = THREE.TextureLoader('textures/earth.jpg')
-
-material.bumpMap    = THREE.TextureLoader('textures/earthbump.jpg')
-material.bumpScale = 0.05
-
-camera.position.z = 5;
+var camera, scene, renderer;
+var geometry, material, mesh, point;
+var canvas;
 
 
-var animate = function () {
-    requestAnimationFrame( animate );
-    onRenderFcts.push(function(delta, now){
-        earthMesh.rotation.y  += 1/32 * delta
-    })
-/*
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-    renderer.render(scene, camera);
+var x = 1;
+var y = 1;
 
-    onRenderFcts.push(function(delta, now){
-        earthMesh.rotation.y  += 1/32 * delta
-    })
-*/
-};
+var data;
 
+init();
+getData();
 animate();
+
+function getData() {
+
+}
+
+
+
+function init() {
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.z = 1;
+
+    scene = new THREE.Scene();
+
+    //Create a new ambient light
+    var light = new THREE.AmbientLight( 0x888888 )
+    scene.add( light )
+
+    //Create a new directional light
+    var light = new THREE.DirectionalLight( 0xfdfcf0, 1 )
+    light.position.set(20,10,20)
+    scene.add( light )
+
+    //Create geometry and material
+    var geometry = new THREE.SphereGeometry(0.5, 32, 32);
+    var texture = new THREE.TextureLoader().load( "../images/earth.jpg" );
+    var material = new THREE.MeshBasicMaterial( {
+        map: texture
+    } );
+
+    point = new THREE.Mesh(geometry);
+
+    //material.map  = THREE.TextureLoader('textures/earth.jpg')
+    material.bumpMap = THREE.TextureLoader('textures/earthbump.jpg')
+    material.bumpScale = 0.05
+
+    /////////////////////////////////
+    ///  GLOBE DOTS ///
+/*
+    var addDot = function(targetX, targetY) {
+
+		// Add a point with zero coordinates
+        var point = new THREE.Vector3(0, 0, 0);
+	geometry.vertices.push(point);
+
+		// Add the coordinates to a new array for the intro animation
+	var result = returnSphericalCoordinates(
+		targetX,
+		targetY
+	);
+
+	animations.dots.points.push(new THREE.Vector3(result.x, result.y, result.z));
+     };
+
+    for (var i = 0; i < data.points.length; i++) {
+	addDot(data.points[i].x, data.points[i].y);
+    }
+
+    for (var country in data.countries) {
+	addDot(data.countries[country].x, data.countries[country].y);
+    }
+
+	// Add the points to the scene
+    groups.globeDots = new THREE.Points(geometry, material);
+    groups.globe.add(groups.globeDots);
+*/
+
+
+    /////////////////////////////////
+
+
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+}
+
+function addPoint(lat, lng, size, color, subgeo) {
+
+    var phi = (90 - lat) * Math.PI / 180;
+    var theta = (180 - lng) * Math.PI / 180;
+
+    point.position.x = 200 * Math.sin(phi) * Math.cos(theta);
+    point.position.y = 200 * Math.cos(phi);
+    point.position.z = 200 * Math.sin(phi) * Math.sin(theta);
+
+    point.lookAt(mesh.position);
+
+    point.scale.z = Math.max( size, 0.1 ); // avoid non-invertible matrix
+    point.updateMatrix();
+
+    for (var i = 0; i < point.geometry.faces.length; i++) {
+
+      point.geometry.faces[i].color = color;
+
+    }
+
+    THREE.GeometryUtils.merge(subgeo, point);
+}
+
+
+function positioning(event) {
+    console.log("mouse: "+event.clientX +","+event.clientY+" window"+ window.innerWidth/2+","+window.innerHeight/2+"\nx and y")
+    if(event.clientX > window.innerWidth/2 && x < 0)
+            x = 1
+
+    if(event.clientX < window.innerWidth/2 && x > 0)
+            x = -1
+
+    if(event.clientY > window.innerHeight/2 && y < 0)
+            y = 1
+
+    if(event.clientY < window.innerHeight/2 && y > 0)
+            y = -1
+
+
+}
+
+
+function animate(e) {
+
+    requestAnimationFrame( animate );
+    document.addEventListener("click", positioning);
+
+    mesh.rotation.x += 0.001*x
+    mesh.rotation.y += 0.001*y
+
+    renderer.render( scene, camera )
+
+}
